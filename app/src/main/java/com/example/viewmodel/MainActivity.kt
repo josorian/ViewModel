1f42f119641e7d2a98e205f35274d42255c8fd5b
josorian <josejaviersoriano54@gmail.com>
2024-02-27 00:05:22 +0100

    Initial commit

---
+++ app/src/main/java/com/example/viewmodel/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.viewmodel
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.viewmodel.ui.GameScreen
+import com.example.viewmodel.ui.theme.ViewModelTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ViewModelTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                ) {
+                    GameScreen()
+                }
+            }
+        }
+    }
+}
